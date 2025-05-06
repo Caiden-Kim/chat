8d1e00c452092cf403a5ee63e3fa1ecaec081ec5
Caiden Kim <kimc1145@oshkosh.k12.wi.us>
2025-05-06 17:37:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+//import { Filter } from "https://www.unpkg.com/browse/bad-words@4.0.0/";
+
+// Getting the elements within the document
+const form = document.getElementById("form");
+const messageButton = document.getElementById("send-button");
+const joinRoomButton = document.getElementById("room-button");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const messageContainer = document.getElementById("message-container");
+
+var name = prompt("What is your name?");
+while (name.trim() === "" || name == null) {
+  name = prompt("What is your name? (Minimum 1 character)");
+}
+
+var sentToday = false;
+var lastSentTime;
+var currentDate;
+
+//const filter = new Filter();
+//filter.addWords(atob("c2lnbWE="));
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  var message = messageInput.value;
+  const room = roomInput.value;
+
+  if (message.trim() === "") return;
+  displayMessage(message);
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+
+  messageInput.value = "";
+});
+
+joinRoomButton.addEventListener("click", () => {
+  console.log("Join room button clicked");
+});
+
+function displayMessage(message) {
+  const now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var meridiem = " am";
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+
+  const formattedDate = `${month}/${day}/${year}`;
+
+  if (!sentToday || formattedDate != currentDate) {
+    const dateDiv = document.createElement("div");
+    dateDiv.textContent = "—————————— " + formattedDate + " ——————————";
+    dateDiv.classList.add("time");
+    messageContainer.append(dateDiv);
+    sentToday = true;
+    currentDate = formattedDate;
+  }
+
+  if (Date.now() - lastSentTime >= 300000 || lastSentTime == null) {
+    const timeDiv = document.createElement("div");
+
+    if (hours > 12) {
+      hours = hours - 12;
+      meridiem = " pm";
+    }
+
+    timeDiv.textContent = hours + ":" + minutes + meridiem;
+    timeDiv.classList.add("time");
+    messageContainer.append(timeDiv);
+  }
+
+  lastSentTime = Date.now();
+
+  const div = document.createElement("div");
+  div.textContent = name + ": " + message;
+  div.classList.add("message");
+  messageContainer.append(div);
+}
